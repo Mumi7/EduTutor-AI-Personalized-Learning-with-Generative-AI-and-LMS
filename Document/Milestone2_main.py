14e50814a1dad5fb93ec41babe18ff659d7a9ce9
Mumi7 <skbasha1463@gmail.com>
2025-07-05 23:47:12 +0530

    Update Milestone2_main.py

--- Document/Milestone2_main.py
+++ Document/Milestone2_main.py
@@ -1,1 +1,10 @@
+from dotenv import load_dotenv
+import os
 
+load_dotenv()
+
+watsonx_api_key = os.getenv("WATSONX_API_KEY")
+pinecone_api_key = os.getenv("PINECONE_API_KEY")
+google_api_key = os.getenv("GOOGLE_API_KEY")
+
+print("Environment variables loaded successfully!")
